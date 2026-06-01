aa98a88e8a940d4c23d5d11bc5329b241e7f3942
accaicedoa <alaniscaicedo@gmail.com>
2026-05-31 23:20:59 -0500

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
     res.status(200).json({
-        mensaje: "Hola desde la nube, somos Alanis Caicedo y Tomás Hidrobo de Ciencias de Datos"
+        mensaje: "Hola desde la nube, somos Alanis Caicedo y Tomás Hidrobo de Ciencias de Datos."
     });
 }
